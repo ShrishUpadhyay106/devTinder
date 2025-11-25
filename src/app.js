63b5dd384537e548f20f3b917b227bf2ab663812
ShrishUpadhyay106 <shrishupadhyay2003@gmail.com>
2025-11-25 13:02:32 +0530

    getAPIssearchuser

--- src/app.js
+++ src/app.js
@@ -3,18 +3,49 @@ const express= require('express');
 const connectDb = require("./config/database");
 const app = express();
 const User= require("./models/user");
-// app.use(express.json());
+app.use(express.json());
+// for specific one user
+app.get("/user",async(req,res)=>{
+
+  try{
+  //   const users= await User.find({emailId:req.body.emailId});
+  // if(users.length ===0){
+  //   res.status(404).send("user not found");
+  // }
+  // else{
+  //   res.send(users);
+  // }
+  
+  // if two users with same emailId 
+     const users= await User.findOne({emailId:req.body.emailId});
+     res.send(users);
+  }
+  catch(err){
+    res.status(400).send('something went wrong ');
+  }
+});
+
+// for all users.
+app.get("/feed",async(req,res)=>{
+
+  try{
+    const users= await User.find({});
+  if(users.length ===0){
+    res.status(404).send("user not found");
+  }
+  else{
+    res.send(users);
+  }
+  }
+  catch(err){
+    res.status(400).send('something went wrong ');
+  }
+});
 
 
 
 app.post("/signup",async(req,res)=>{
-  const users= new User({
-    firstName: "shrish",
-    lastName: "upadhyay",
-    emailId:"gfhghjb",
-    password: "avbvs",
-    age: 22,
-  });
+  const users= new User(req.body); 
   try{
      await users.save();
  res.send("user added successfully");
