2bf8967bbf864bfb3dc2981a3e3307d26f38d71a
ShrishUpadhyay106 <shrishupadhyay2003@gmail.com>
2025-11-26 17:48:54 +0530

    api level sanitations

--- src/app.js
+++ src/app.js
@@ -1,68 +1,96 @@
 // console.log("starting a new project");
-const express= require('express');
+const express = require("express");
 const connectDb = require("./config/database");
 const app = express();
-const User= require("./models/user");
+const User = require("./models/user");
 app.use(express.json());
 // for specific one user
-app.get("/user",async(req,res)=>{
+app.get("/user", async (req, res) => {
+  try {
+    //   const users= await User.find({emailId:req.body.emailId});
+    // if(users.length ===0){
+    //   res.status(404).send("user not found");
+    // }
+    // else{
+    //   res.send(users);
+    // }
 
-  try{
-  //   const users= await User.find({emailId:req.body.emailId});
-  // if(users.length ===0){
-  //   res.status(404).send("user not found");
-  // }
-  // else{
-  //   res.send(users);
-  // }
-  
-  // if two users with same emailId 
-     const users= await User.findOne({emailId:req.body.emailId});
-     res.send(users);
-  }
-  catch(err){
-    res.status(400).send('something went wrong ');
+    // if two users with same emailId
+    const users = await User.findOne({ emailId: req.body.emailId });
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong ");
   }
 });
 
 // for all users.
-app.get("/feed",async(req,res)=>{
-
-  try{
-    const users= await User.find({});
-  if(users.length ===0){
-    res.status(404).send("user not found");
-  }
-  else{
-    res.send(users);
-  }
-  }
-  catch(err){
-    res.status(400).send('something went wrong ');
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong ");
   }
 });
 
-
-
-app.post("/signup",async(req,res)=>{
-  const users= new User(req.body); 
-  try{
-     await users.save();
- res.send("user added successfully");
-  }
-  catch(err){
-    res.status(400).send('error saving the data '+ err.message);
+app.delete("/user", async (req, res) => {
+  try {
+    const userid = req.body.userId;
+    const user = await User.findByIdAndDelete({ _id: userid });
+    res.send("user delete succesfully");
+  } catch (err) {
+    res.status(400).send("something went wrong ");
   }
 });
 
-connectDb().then(()=> {
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  
+  try {
+    const ALLOWED_UPADTES = ["age", "gender", "skills", "photourl"];
+
+  const isUpdateAllowed = Object.keys(data).every((k) =>
+    ALLOWED_UPADTES.includes(k)
+  );
+  if (!isUpdateAllowed) {
+    throw new Error("Upadate not allowed");
+  }
+  if(data.skills.length>=10){
+    throw new Error("skills more than 10 not allowed");
+  }
+    const user= await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("updated successfully");
+  } catch (err) {
+    res.status(400).send("update failed " + err.message);
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const users = new User(req.body);
+  try {
+    await users.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(400).send("error saving the data " + err.message);
+  }
+});
+
+connectDb()
+  .then(() => {
     console.log("database connection established");
-    app.listen(3000 ,()=>{
-    console.log("server is sucessfully listeing on the port 3000")
-});
-}).catch(err=>{
-  console.error("database cannot be connected!!")
-});
-
-
-
+    app.listen(3000, () => {
+      console.log("server is sucessfully listeing on the port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("database cannot be connected!!");
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,51 @@
 const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName : {
         type: String,
+        required: true,
+        minLength:4,
     },
     lastName :{
         type: String,
     },
     emailId: {
         type: String,
+        required: true,
+        unique:true,
+        lowercase:true,
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        min:18,
     },
+    gender:{
+        type: String,
+        // it only run by default for new user only not for updation so you have to enable it for updation in update api
+        validate(value){
+            if(!["male","female"].includes(value)){
+                throw new Error("gender data is insufficient");
+            }
+        },
+    },
+    photoUrl:{
+        type: String,
+    },
+    about :{
+        type: String,
+        default:"this is defult value "
+    },
+    skills:{
+        // array of strings for skills
+        type: [String],
+    },
+    
+},
+{
+    timestamps:true
 });
 
 const User = mongoose.model("User",userSchema);
